20c3eb1859eb5edf8bd1d5d2e0be2b4134768156
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-03-11 06:40:41 +0700

    Create Entry Component

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,6 +1,13 @@
 import Header from "./components/Header.jsx"
+import Entry from "./components/Entry.jsx"
+
 export default function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main className="container">
+                <Entry />
+            </main>
+        </>
     )
 }
